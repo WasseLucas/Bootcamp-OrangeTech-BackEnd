20b3c7c6039c29eaf8e89e1a8a4e893c96c5a8e2
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-02 12:28:18 -0300

    media estudantil

---
+++ src/atividadesDiarias/BoletimStudantil.java
@@ -0,0 +1,19 @@
+package atividadesDiarias;
+
+import javax.sound.midi.Soundbank;
+
+public class BoletimStudantil {
+  public static void main(String[] args) {
+    // Fazer um programa para saber se o aluno está aprovado ou reporovado.
+
+    int mediaFinal = 6;
+
+    if( mediaFinal < 6){
+        System.out.println("REPORVADO");
+    }else if( mediaFinal == 6){
+        System.out.println("PROVA MINERVA");
+    } else{
+         System.out.println("APROVADO");
+    }
+  }
+}
