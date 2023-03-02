9132168a72665d81bfd09d11e7a70c18027219b2
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-02 14:36:02 -0300

    exemplo para comparar um objeto usando eguals

---
+++ src/atividadesDiarias/ComparandoObjetos.java
@@ -0,0 +1,12 @@
+package atividadesDiarias;
+
+public class ComparandoObjetos {
+  public static void main(String[] args) {
+    
+    String nomeUm = "WASSE";
+    String nomeDois = new String("WASSE");
+
+    System.out.println(nomeUm.equals(nomeDois));
+
+  }
+}
