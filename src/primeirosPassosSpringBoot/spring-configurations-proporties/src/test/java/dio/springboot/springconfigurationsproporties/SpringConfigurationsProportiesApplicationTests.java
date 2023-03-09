165831bd409fd9c37045f607430636603e310132
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-09 17:46:58 -0300

    configuration proporties

---
+++ src/primeirosPassosSpringBoot/spring-configurations-proporties/src/test/java/dio/springboot/springconfigurationsproporties/SpringConfigurationsProportiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.springconfigurationsproporties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationsProportiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
