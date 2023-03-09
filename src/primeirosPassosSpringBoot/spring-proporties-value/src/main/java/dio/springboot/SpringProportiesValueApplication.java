594cc6c4d4ba7af9d6376e96ada3c4aba5b5d8ca
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-09 13:33:33 -0300

    sistema de mensagem ultilizando proporties value

---
+++ src/primeirosPassosSpringBoot/spring-proporties-value/src/main/java/dio/springboot/SpringProportiesValueApplication.java
@@ -0,0 +1,12 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProportiesValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProportiesValueApplication.class, args);
+	}
+}
---
+++ src/primeirosPassosSpringBoot/spring-proporties-value/src/test/java/dio/springboot/springproportiesvalue/SpringProportiesValueApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.springproportiesvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProportiesValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
