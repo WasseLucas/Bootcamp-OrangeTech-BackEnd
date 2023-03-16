b70be432288ec9f2e772160a4af08991d3c5e940
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-16 15:27:44 -0300

    conectando banco de dados application.properties

--- src/primeirosPassosSpringBoot/spring-data-jpa/src/main/java/dio/springdatajpa/SpringDataJpaApplication.java
+++ src/primeirosPassosSpringBoot/spring-data-jpa/src/main/java/dio/springdatajpa/SpringDataJpaApplication.java
@@ -2,9 +2,14 @@ package dio.springdatajpa.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "dio.springdatajpa.app.StartApp")
+@EntityScan(basePackages = "dio.springdatajpa.model.User")
+@EnableJpaRepositories(basePackages = "dio.springdatajpa.repository.UserRepository")
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
