d90e588cd4672fb4f7069e0c921b9e006a0d1880
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-16 13:39:03 -0300

    Spring data-jpa

---
+++ src/primeirosPassosSpringBoot/spring-data-jpa/src/main/java/dio/springdatajpa/app/StartaApp.java
@@ -0,0 +1,32 @@
+package dio.springdatajpa.app;
+
+import dio.springdatajpa.model.User;
+import dio.springdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StartaApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("Wasse Lucas");
+        user.setUserName("wassejp");
+        user.setPassword("wasselucas01");
+
+        repository.save(user);
+
+        for (User u : repository.findAll()){
+            System.out.println(u);
+        }
+
+    }
+}
