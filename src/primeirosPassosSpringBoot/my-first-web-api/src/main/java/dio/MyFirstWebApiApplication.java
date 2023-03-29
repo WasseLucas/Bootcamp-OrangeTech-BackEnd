c2b6809afd5b34680455ba35f8a9534edadc82aa
Wasse Lucas <wasselucas.dev@gmail.com>
2023-03-29 16:12:36 -0300

    configuraacao swagger spring boot 3.0.5

--- src/primeirosPassosSpringBoot/my-first-web-api/src/main/java/dio/MyFirstWebApiApplication.java
+++ src/primeirosPassosSpringBoot/my-first-web-api/src/main/java/dio/MyFirstWebApiApplication.java
@@ -1,5 +1,8 @@
 package dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +11,14 @@ import org.springframework.context.annotation.ComponentScans;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Mr Java",
+						email = "seuemail@gmail.com"
+				)
+		)
+)
 public class MyFirstWebApiApplication {
 
 	public static void main(String[] args) {
